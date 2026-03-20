4ecef945489a61ebc3cc7bad2ca06a1c1bf1ecde
Einstizin <dcsgames0801@gmail.com>
2026-03-20 15:37:22 -0300

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,26 @@
-const m=require('mongoose');
-module.exports=m.model('User',new m.Schema({
-name:String,email:String,password:String,role:{type:String,default:'paciente'}
-}));
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model(
+  'User',
+  new mongoose.Schema({
+    name: String,
+    email: {
+      type: String,
+      unique: true
+    },
+    password: String,
+    role: {
+      type: String,
+      enum: ['paciente', 'secretario'],
+      default: 'paciente'
+    },
+    emailVerified: {
+      type: Boolean,
+      default: false
+    },
+    emailVerificationToken: {
+      type: String,
+      default: null
+    }
+  })
+);
