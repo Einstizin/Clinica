7cae4c8ff9ea8f6a0d2e19644fa15abd475406a5
Einstizin <dcsgames0801@gmail.com>
2026-03-20 14:01:31 -0300

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -209,26 +209,3 @@ li {
   margin-bottom: 8px;
 }
 </style>
-
-<script>
-import axios from 'axios';
-export default{
-data(){return{email:'',password:'',token:'',date:'',cep:'',endereco:'',lista:[]}},
-methods:{
-async login(){
-const r=await axios.post('https://clinica-wkzp.onrender.com/auth/login',{email:this.email,password:this.password});
-this.token=r.data.token;},
-async agendar(){
-await axios.post('https://clinica-wkzp.onrender.com/appointments',{date:this.date,address:this.endereco},{
-headers:{Authorization:this.token}});
-alert('ok');this.carregar();},
-async carregar(){
-const r=await axios.get('https://clinica-wkzp.onrender.com/appointments',{headers:{Authorization:this.token}});
-this.lista=r.data;},
-async buscarCep(){
-const r=await axios.get('https://clinica-wkzp.onrender.com/external/cep/'+this.cep);
-this.endereco=r.data.logradouro;}
-},
-mounted(){this.carregar();}
-}
-</script>
