d2bdf146c236c2330c57dc7bace26ae3a5951c48
Einstizin <dcsgames0801@gmail.com>
2026-03-20 13:29:53 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,4 +4,5 @@ const auth=require('./routes/auth');const appt=require('./routes/appointments');
 const app=express();app.use(cors());app.use(express.json());
 mongoose.connect(process.env.MONGO_URI).then(()=>console.log('DB ok'));
 app.use('/auth',auth);app.use('/appointments',appt);app.use('/external',ext);
-app.listen(3000,()=>console.log('rodando 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('rodando na porta', PORT));
