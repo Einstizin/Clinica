761fa657be0d6ec1cc53cbdf2419550046832700
Einstizin <dcsgames0801@gmail.com>
2026-03-20 14:04:10 -0300

    Add VCalendar to Vue app initialization

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,2 +1,6 @@
-import {createApp} from 'vue';import App from './App.vue';
-createApp(App).mount('#app');
+import { createApp } from 'vue'
+import App from './App.vue'
+import VCalendar from 'v-calendar'
+import 'v-calendar/style.css'
+
+createApp(App).use(VCalendar, {}).mount('#app')
